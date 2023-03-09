6127d633e7f1bf1450415fd2c3c2309b0b763cfd
karthik1102spark <karthik.a@sparkouttech.com>
2023-03-09 22:34:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+//core modules or built in modules
+var http = require('http');
+http.createServer(function (req, res) {
+    console.log('localhost running');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Welcome to this page!');
+  res.end();
+}).listen(3000);
+//--------------------------------------------------
+
+// Third-party Modules or external modules
+var lodash = require('lodash');
+
+var users = [
+    { 'user': 'barney',  'active': true },
+  { 'user': 'fred',    'active': false },
+  { 'user': 'pebbles', 'active': false }
+  ];
+
+let a = lodash.findLastIndex(users, ['active', false]);
+// console.log(a);
+// => 2
+let b = lodash.findLastIndex(users, 'active');
+// console.log(b);
+// => 0
+//--------------------------------------------------
+
+//local modules or custom modules
+var calculator = require('./calc'); 
+    
+var x = 50, y = 10; 
+    
+console.log("Addition of 50 and 10 is "
+                   + calculator.add(x, y)); 
+    
+// console.log("Subtraction of 50 and 10 is "
+//                    + calculator.sub(x, y)); 
+    
+// console.log("Multiplication of 50 and 10 is "
+//                    + calculator.mult(x, y)); 
+    
+// console.log("Division of 50 and 10 is " 
+//                    + calculator.div(x, y)); 
